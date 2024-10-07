1d849d3923bebfbb7c0b75bfc6c03033f3769138
Josechav12 <josechavarri12@gmail.com>
2024-10-07 21:47:18 +0000

    terminamos el punto_E

---
+++ RTA_SCRIPT_Examen_20241006/Punto_E.sh
@@ -0,0 +1,16 @@
+# entramos al archivo RTA_ARCHIVOS_Examen_20240928/
+cd /repogit/RTA_ARCHIVOS_Examen_20240928/
+
+# creamos el archivo .txt si no existe
+touch Filtro_Basico.txt
+
+#extraemos la informacion de la memoria
+echo "Actualizando información de la memoria RAM"
+grep MemTotal /proc/meminfo > Filtro_Basico.txt
+
+# Extraer la información del fabricante del chassis
+sudo dmidecode -t chassis | grep "Manufacturer:" >> Filtro_Basico.txt
+
+
+
+
